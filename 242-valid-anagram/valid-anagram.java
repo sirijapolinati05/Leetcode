cc1a3e6b9550f3c29e3cf71523fcd44ea3f0ad6f
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-04-25 09:53:11 +0530

    Time: 74 ms (5.44%) | Memory: 54.4 MB (7.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
         if(s.length() != t.length())
         {
             return false;
