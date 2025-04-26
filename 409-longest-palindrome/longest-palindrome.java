b323a53378cb0f2750d179ea83b138cc36a0aa51
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-04-26 21:17:08 +0530

    Time: 7 ms (51.01%) | Memory: 42 MB (44.86%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> count = new HashMap();
+        for(char ch: s.toCharArray())
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
+        
+        int result = 0;
+        boolean odd_found = false;
+        for(int ch: count.values())
+        {
+            if(ch % 2 == 0) 
+            {
+                result += ch;
+            }
+            else
+            {
+                odd_found = true;
+                result += ch - 1;
+            }
+        }
+        if(odd_found) 
+        {
+            result++;
+        }
+        return result;
+    }
+}
