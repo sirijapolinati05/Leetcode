b63478592c3cc01d61af1537b70a18f32c3fdc82
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-04-26 14:47:38 +0530

    Time: 2 ms (66.46%) | Memory: 41.6 MB (54.97%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        if (t.length() < s.length()) 
+        {
+            return false;
+        }
+
         int i = 0;
         int j = 0;
 
