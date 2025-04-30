50a49ed6d6454d9793ed3efdb9f0700ca538a934
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-04-30 12:36:22 +0530

    Time: 7 ms (16.02%) | Memory: 44.7 MB (7.80%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,23 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0)
-        {
-            return false;
-        }
         String s = Integer.toString(x);
-
-        int left = 0;
-        int right = s.length() - 1;
-
-        while(left < right)
-        {
-            if(s.charAt(left) != s.charAt(right))
-            {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        String reversed = new StringBuilder(s).reverse().toString();
+        return s.equals(reversed);
     }
 }
