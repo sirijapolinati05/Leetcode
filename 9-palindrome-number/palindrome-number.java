85f707046ea0e74502d67427b64c72c9c88fb663
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-04-30 12:36:57 +0530

    Time: 4 ms (100.00%) | Memory: 44.4 MB (17.99%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,7 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = Integer.toString(x);
-        String reversed = new StringBuilder(s).reverse().toString();
-        return s.equals(reversed);
+        // Negative numbers and numbers ending in 0 (but not 0 itself) can't be palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+
+        while (x > reversed) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+
+        // Check for even and odd length palindromes
+        return (x == reversed || x == reversed / 10);
     }
 }
