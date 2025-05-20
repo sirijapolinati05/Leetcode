5b8067952bff21ae7010352a1321e45f36bf44f7
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-05-20 20:55:00 +0530

    Time: 15 ms (22.12%) | Memory: 49.5 MB (86.54%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for(int num : nums)
+        {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if(map.get(num) > n / 2)
+            {
+                return num;
+            }
+        }
+        return -1;
     }
 }
