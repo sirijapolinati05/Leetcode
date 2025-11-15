1f88e81bff3f7373a74d172471cbc0ed6866909c
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-11-15 20:13:17 +0530

    Time: 5 ms (55.90%) | Memory: 46.9 MB (5.50%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,36 +1,29 @@
-import java.util.HashMap;
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
 
-class Solution 
-{
-    public int romanToInt(String s) 
-    {
-        HashMap<Character, Integer> roman = new HashMap<>();
+        
+        hash.put('I', 1);
+        hash.put('V', 5);
+        hash.put('X', 10);
+        hash.put('L', 50);
+        hash.put('C', 100);
+        hash.put('D', 500);
+        hash.put('M', 1000);
 
-        roman.put('I',1);
-        roman.put('V',5);
-        roman.put('X',10);
-        roman.put('L',50);
-        roman.put('C',100);
-        roman.put('D',500);
-        roman.put('M',1000);
+        int result = hash.get(s.charAt(s.length() - 1));
 
-        int total = 0;
-
-        int length = s.length();
-
-        for(int i = 0; i < length; i++) 
+        for(int i = s.length() - 2; i >= 0; i--)
         {
-            int current = roman.get(s.charAt(i));
-
-            if(i < length - 1 && roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) 
+            if(hash.get(s.charAt(i)) < hash.get(s.charAt(i + 1)))
             {
-                total = total - current;
+                result = result - hash.get(s.charAt(i));
             }
-            else 
+            else
             {
-                total = total + current;
+                result = result + hash.get(s.charAt(i));
             }
         }
-        return total;
+        return result;
     }
 }
