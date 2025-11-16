97316775ba2616fd9075511a999d3b1faebd1ffa
POLINATI SIRIJA <sirijapolinati17@gmail.com>
2025-11-16 20:12:51 +0530

    Time: 845 ms (82.48%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT Employee.name, Bonus.bonus
+FROM Employee
+LEFT JOIN
+Bonus
+ON Employee.empId = Bonus.empId
+WHERE
+Bonus.bonus < 1000 OR Bonus.bonus IS NULL;
+
